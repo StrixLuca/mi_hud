7f303c9db5e984ea7897cf043d291fa6cd9078ed
StrixLuca <110634650+StrixLuca@users.noreply.github.com>
2023-10-11 21:36:55 +0200

    Update client.lua

--- bridge/qb/client.lua
+++ bridge/qb/client.lua
@@ -1,15 +1,20 @@
-        --[[    client bridge file: qb    ]]--
--- client methods to adapt to the named framework
-------------------------------------------------------------
--- docs reference: https://docs.qbcore.org/qbcore-documentation
-
--- framework variables
-----------------------------------------
+ 
+       --[[    client bridge file: qb    ]]--
+if GetResourceState('qb-core') ~= 'started' then return end   --- if QBCore start then use qb bridge
 
 
--- local functions
-----------------------------------------
 
 
--- main functions
-----------------------------------------
+local QBCore = exports['qb-core']:GetCoreObject()     ---- Core export
+local PlayerData = QBCore.Functions.GetPlayerData()   ---- GetPlayerData
+
+
+function ServerCallback(name, cb, ...)             ----- callback
+    QBCore.Functions.TriggerCallback(name, cb,  ...)
+end
+
+function Notify(text)                   ------ Notify
+	QBCore.Functions.Notify(text)
+end
+
+
