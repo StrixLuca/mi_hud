4bdc0107f56584b8b4ed6c87003b5d8a15b8f48e
StrixLuca <110634650+StrixLuca@users.noreply.github.com>
2023-10-12 11:52:35 +0200

    Update server.lua

--- bridge/esx/server.lua
+++ bridge/esx/server.lua
@@ -1,7 +1,13 @@
         --[[    server bridge file: esx    ]]--
 -- server methods to adapt to the named framework
 ------------------------------------------------------------
--- docs reference: https://documentation.esx-framework.org
+if GetResourceState('es_extended') ~= 'started' then return end
+
+ESX = exports.es_extended:getSharedObject()
+
+function RegisterCallback(name, cb)
+    ESX.RegisterServerCallback(name, cb)
+end
 
 -- framework variables
 ----------------------------------------
